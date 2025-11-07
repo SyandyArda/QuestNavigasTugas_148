17cd6ee19fd679d0e369bdba41e346a5c151765e
Arsyan <syandi.2901@gmail.com>
2025-11-06 23:33:58 -0800

    menambahkan scaffold serta memodifikasi layout di tampildata

--- app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
@@ -26,3 +26,60 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tugasnavigasi.R
 import com.example.tugasnavigasi.data.CobaUiState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanData(
+    uiState: CobaUiState, // Terima UiState dari ViewModel
+    onBerandaBtnClick: () -> Unit,
+    onFormulirBtnClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        // Judul sesuai string resource "List Daftar Peserta"
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = modifier
+                .padding(isiRuang)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            // Tampilkan data dari ViewModel, bukan hardcode
+            // Gunakan Card agar mirip dengan desain di gambar
+            TampilDataField(label = stringResource(id = R.string.nama_lengkap), data = uiState.nama)
+            TampilDataField(label = stringResource(id = R.string.jenis_kelamin), data = uiState.jenisKelamin)
+            TampilDataField(label = stringResource(id = R.string.status_perkawinan), data = uiState.status) // Tambahkan string ini
+            TampilDataField(label = stringResource(id = R.string.alamat), data = uiState.alamat)
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            // --- TOMBOL NAVIGASI (Sesuai Gambar 2 & 4) ---
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBerandaBtnClick // Navigasi ke Welcome
+            ) {
+                Text(text = stringResource(id = R.string.beranda)) // Tambahkan string ini
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onFormulirBtnClick // Navigasi kembali ke Formulir
+            ) {
+                Text(text = stringResource(id = R.string.formulir_pendaftaran)) // Tambahkan string ini
+            }
+        }
+    }
+}
