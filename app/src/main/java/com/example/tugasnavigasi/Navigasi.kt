7c9369fd0e971f25207f8752b989a8dce617e4f8
Arsyan <syandi.2901@gmail.com>
2025-11-06 23:59:10 -0800

    menambahkan import untuk di navigasi

--- app/src/main/java/com/example/tugasnavigasi/Navigasi.kt
+++ app/src/main/java/com/example/tugasnavigasi/Navigasi.kt
@@ -1,2 +1,14 @@
 package com.example.tugasnavigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.tugasnavigasi.data.DataViewModel
+import com.example.tugasnavigasi.view.HalamanData
+import com.example.tugasnavigasi.view.HalamanFormulir
+import com.example.tugasnavigasi.view.HalamanWelcome
