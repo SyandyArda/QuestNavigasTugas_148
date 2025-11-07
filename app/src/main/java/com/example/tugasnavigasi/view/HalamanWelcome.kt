b85d9276e781bcf109e92b3f729103a05d0ea671
Arsyan <syandi.2901@gmail.com>
2025-11-06 23:47:39 -0800

    menambahkan fungsi di halamanwelcome

--- app/src/main/java/com/example/tugasnavigasi/view/HalamanWelcome.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/HalamanWelcome.kt
@@ -19,3 +19,41 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tugasnavigasi.R
 
+@Composable
+fun HalamanWelcome(
+    onNextButtonClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Selamat Datang",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+
+
+        Image(
+            painter = painterResource(id = R.drawable.nagi),
+            contentDescription = "Logo Card-Ist",
+            modifier = Modifier.width(150.dp) //
+        )
+        Text(text = "MOBILE APP 2025") //
+
+        Spacer(modifier = Modifier.height(100.dp))
+
+        Text(text = "Nagi Seishiro", fontSize = 18.sp)
+        Text(text = "20230140148", fontSize = 16.sp)
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(onClick = onNextButtonClicked) {
+            Text(text = "Submit")
+        }
+    }
+}
